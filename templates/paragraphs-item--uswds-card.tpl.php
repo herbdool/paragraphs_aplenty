9b9dc5c35b96d876b557f9ced5a0b4f6fd8909c9
Herb v/d Dool <herb@3speedhub.com>
2024-03-07 13:56:39 -0500

    Use core grid layout for cards

--- templates/paragraphs-item--uswds-card.tpl.php
+++ templates/paragraphs-item--uswds-card.tpl.php
@@ -1,3 +1,5 @@
 <div class="<?php print implode(' ', $classes); ?>"<?php print backdrop_attributes($attributes); ?>>
-  <?php print render($content) ?>
+  <div class="content"<?php print backdrop_attributes($content_attributes); ?>>
+    <?php print render($content) ?>
+  </div>
 </div>
