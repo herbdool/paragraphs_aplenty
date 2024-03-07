9766b6adf9acf959cc8945e4299d4065fccf146e
Herb v/d Dool <herb@3speedhub.com>
2024-03-07 17:23:14 -0500

    use <figure> for image

---
+++ templates/paragraphs-item--uswds-image.tpl.php
@@ -0,0 +1,11 @@
+<div class="<?php print implode(' ', $classes_array); ?>"<?php print backdrop_attributes($attributes); ?>>
+  <div class="content"<?php print backdrop_attributes($content_attributes); ?>>
+    <figure>
+      <?php print render($content['field_uswds_image']); ?>
+      <figcaption>
+        <?php print render($caption); ?>
+        <?php print render($caption_link); ?>
+      </figcaption>
+    </figure>
+  </div>
+</div>
