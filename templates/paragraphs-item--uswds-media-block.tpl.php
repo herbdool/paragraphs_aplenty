a0d8b32c5a2b859290dbe4e9176b228979864752
Herb v/d Dool <herb@3speedhub.com>
2024-03-07 15:58:49 -0500

    Add a media align to media blocks

--- templates/paragraphs-item--uswds-media-block.tpl.php
+++ templates/paragraphs-item--uswds-media-block.tpl.php
@@ -1,10 +1,11 @@
 <div class="<?php print implode(' ', $classes); ?>"<?php print backdrop_attributes($attributes); ?>>
-  <div class="aplenty-media_block-image">
-    <?php print render($content['field_uswds_image'][0]) ?>
-  </div>
-  <div class="aplenty-media_block-body">
-    <h2><?php print render($content['field_uswds_title'][0]) ?></h2>
-    <?php print render($content['field_uswds_body'][0]) ?>
+  <div class="content"<?php print backdrop_attributes($content_attributes); ?>>
+    <div class="aplenty-media_block-image">
+      <?php print render($content['field_uswds_image'][0]) ?>
+    </div>
+    <div class="aplenty-media_block-body">
+      <h2><?php print render($content['field_uswds_title'][0]) ?></h2>
+      <?php print render($content['field_uswds_body'][0]) ?>
+    </div>
   </div>
 </div>
-
