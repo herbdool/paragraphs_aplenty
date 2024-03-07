75475545003715248d132ecd9ca360f0ddc4ce4e
Herb v/d Dool <herb@3speedhub.com>
2024-03-07 17:05:05 -0500

    remove id on accordion item

--- templates/paragraphs-item--uswds-accordion-item.tpl.php
+++ templates/paragraphs-item--uswds-accordion-item.tpl.php
@@ -1,11 +1,9 @@
 <details class="<?php print implode(' ', $classes_array); ?>"<?php print backdrop_attributes($attributes); ?> <?php print $expanded; ?>>
-
   <summary>
     <?php print $accordion_title ?>
   </summary>
-  <div id="<?php print $controls ?>" class="aplenty-accordion-content">
+  <div class="aplenty-accordion-content">
     <?php hide($content['field_uswds_title']); ?>
     <?php print render($content) ?>
   </div>
-
 </details>
