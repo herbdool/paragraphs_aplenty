75d8e3d2fffbd4ee9c8a39db7b69d2e3d014df02
Herb v/d Dool <herb@3speedhub.com>
2024-03-08 00:06:42 -0500

    Add media width field and rename media list

--- templates/paragraphs-item--aplenty-graphic-list.tpl.php
+++
@@ -1,9 +0,0 @@
-<div class="<?php print implode(' ', $classes_array); ?>"<?php print backdrop_attributes($attributes); ?>>
-  <div class="content"<?php print backdrop_attributes($content_attributes); ?>>
-      <?php foreach ($aplenty_graphic_list_rows as $graphic_list_row): ?>
-        <div class="aplenty-graphic_list-row">
-          <?php print render($graphic_list_row) ?>
-        </div>
-      <?php endforeach; ?>
-  </div>
-</div>
---
+++ templates/paragraphs-item--aplenty-media-list.tpl.php
@@ -0,0 +1,12 @@
+<div class="<?php print implode(' ', $classes_array); ?>"<?php print backdrop_attributes($attributes); ?>>
+  <div class="content"<?php print backdrop_attributes($content_attributes); ?>>
+    <div class="container">
+      <div class="row">
+        <?php foreach ($aplenty_media_list as $media_item): ?>
+        <?php print render($media_item); ?>
+        <?php endforeach; ?>
+      </div>
+    </div>
+  </div>
+
+</div>
