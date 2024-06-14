98f85423f35e82595f53ee4277b46ce49b1748ec
Herb v/d Dool <herb@3speedhub.com>
2024-06-14 16:35:26 -0400

    Issue #6: Create automatic id from heading

--- templates/paragraphs-item--aplenty-heading.tpl.php
+++ templates/paragraphs-item--aplenty-heading.tpl.php
@@ -1,7 +1,6 @@
 <div class="<?php print implode(' ', $classes_array); ?>"<?php print backdrop_attributes($attributes); ?>>
   <div class="aplenty-heading">
-    <a id="<?php print $anchor_text ?>"></a>
-    <h2><?php print $heading_text ?></h2>
+    <?php print render($heading); ?>
   </div>
 </div>
 
