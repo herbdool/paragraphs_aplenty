577a036c24cf9da03690b032ccc8b3d8176f8e1f
Herb <herb@3speedhub.com>
2024-09-20 10:59:26 -0400

    Issue #27 fix some styles (#34)

--- templates/paragraphs-item--aplenty-grid.tpl.php
+++ templates/paragraphs-item--aplenty-grid.tpl.php
@@ -3,10 +3,12 @@
   <?php print render($title_prefix); ?>
   <?php print render($title_suffix); ?>
   <div class="content"<?php print backdrop_attributes($content_attributes); ?>>
-    <div class="row">
-      <?php foreach ($aplenty_columns as $column): ?>
-      <?php print render($column); ?>
-      <?php endforeach; ?>
+    <div class="container-fluid">
+      <div class="row">
+        <?php foreach ($aplenty_columns as $column): ?>
+        <?php print render($column); ?>
+        <?php endforeach; ?>
+      </div>
     </div>
   </div>
 
--- templates/paragraphs-item--aplenty-heading.tpl.php
+++ templates/paragraphs-item--aplenty-heading.tpl.php
@@ -2,8 +2,10 @@
   <?php print render($admin_links); ?>
   <?php print render($title_prefix); ?>
   <?php print render($title_suffix); ?>
-  <div class="aplenty-heading">
-    <?php print render($heading); ?>
+  <div class="content"<?php print backdrop_attributes($content_attributes); ?>>
+    <div class="aplenty-heading">
+      <?php print render($heading); ?>
+    </div>
   </div>
 </div>
 
