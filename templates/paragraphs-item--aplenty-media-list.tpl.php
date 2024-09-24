26cfb2be484b3af2fbfcc1378a881f5cc1cbd517
Herb <herb@3speedhub.com>
2024-09-24 14:07:07 -0400

    Update paragraphs-item--aplenty-media-list.tpl.php

--- templates/paragraphs-item--aplenty-media-list.tpl.php
+++ templates/paragraphs-item--aplenty-media-list.tpl.php
@@ -3,7 +3,7 @@
   <?php print render($title_prefix); ?>
   <?php print render($title_suffix); ?>
   <div class="content"<?php print backdrop_attributes($content_attributes); ?>>
-    <div class="row">
+    <div class="grid">
       <?php foreach ($aplenty_media_list as $media_item): ?>
       <?php print render($media_item); ?>
       <?php endforeach; ?>
