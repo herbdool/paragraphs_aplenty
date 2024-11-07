f029ebe530aa180b2e67197e99c363964f3a95f0
Herb v/d Dool <herb@3speedhub.com>
2024-11-07 11:19:34 -0500

    Issue #46: choose between row/grid. Full-width content

--- templates/paragraphs-item--aplenty-grid.tpl.php
+++ templates/paragraphs-item--aplenty-grid.tpl.php
@@ -3,7 +3,7 @@
   <?php print render($title_prefix); ?>
   <?php print render($title_suffix); ?>
   <div class="content"<?php print backdrop_attributes($content_attributes); ?>>
-    <div class="row grid">
+    <div class="<?php print $aplenty_row_class; ?>">
       <?php foreach ($aplenty_columns as $column): ?>
         <?php print render($column); ?>
       <?php endforeach; ?>
